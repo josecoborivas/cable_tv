41e7674a919c06e2da18c36f34736173b6d3c6eb
josecoborivas <josecoborivas@gmail.com>
2021-09-08 11:30:43 -0300

    init commit

---
+++ src/views/Index.vue
@@ -0,0 +1,258 @@
+<template>
+  <div class="wrapper">
+    <parallax class="page-header header-filter" :style="headerStyle">
+      <div class="md-layout">
+        <div class="md-layout-item">
+          <div class="image-wrapper">
+            
+            <div class="brand">
+              <h1>Home Tv</h1>
+              <h3>Tv Cable gratis sin anuncios.</h3>
+            </div>
+          </div>
+        </div>
+      </div>
+    </parallax>
+    <div class="main main-raised">
+      <div class="section section-basic">
+        <div class="container">
+          <div class="title">
+            <h2 class="mb-4">Canales</h2>
+            <v-row class="d-flex justify-start mb-6 mt-4" justify="center" align="center">
+              
+              <v-col cols="3" md="3" class="ml-4 mr-4" style="display: flex; align-items: center;">
+                <img style="width: 200px; heigth:200px; cursor: pointer;" link @click="ver('discoveryTurbo')" src="@/assets/img/logos/discoveryTurbo.png" alt="">  
+              </v-col>
+
+              <v-col cols="3" md="3" style="display: flex; align-items: center;">
+                <img style="width: 200px; heigth:200px; cursor: pointer;" link @click="ver('h&h')" src="@/assets/img/logos/h&h.png" alt="">
+              </v-col>
+
+              <v-col cols="3" md="3" style="display: flex; align-items: center;">
+                <img style="width: 200px; heigth:200px; cursor: pointer;" link @click="ver('discoveryID')" src="@/assets/img/logos/discoveryID.png" alt="">
+              </v-col>
+
+              <v-col cols="3" md="3" style="display: flex; align-items: center;">
+                <img style="width: 200px; heigth:200px; cursor: pointer;" link @click="ver('discoveryChannel')" src="@/assets/img/logos/discoveryChannel.png" alt="">
+              </v-col>
+
+              <v-col cols="3" md="3" align="center" style="display: flex; align-items: center;">
+                <img style="width: 200px; heigth:200px; cursor: pointer;" link @click="ver('discoveryWorld')" src="@/assets/img/logos/discoveryWorld.png" alt="">
+              </v-col>
+
+              <v-col cols="3" md="3" style="display: flex; align-items: center;">
+                <img style="width: 200px; heigth:200px; cursor: pointer;" link @click="ver('discoveryTheater')" src="@/assets/img/logos/discoveryTheater.png" alt="">
+              </v-col>
+
+              <v-col cols="3" md="3" style="display: flex; align-items: center;">
+                <img style="width: 200px; heigth:200px; cursor: pointer;" link @click="ver('natgeo')" src="@/assets/img/logos/natgeo.png" alt="">
+              </v-col>
+
+              <v-col cols="3" md="3" style="display: flex; align-items: center;">
+                <img style="width: 200px; heigth:200px; cursor: pointer;" link @click="ver('axn')" src="@/assets/img/logos/axn.png" alt="">
+              </v-col>
+
+              <v-col cols="3" md="3" style="display: flex; align-items: center;">
+                <img style="width: 200px; heigth:200px; cursor: pointer;" link @click="ver('vtv')" src="@/assets/img/logos/vtv.png" alt="">
+              </v-col>
+
+              <v-col cols="3" md="3" align="center" style="display: flex; align-items: center;">
+                <img style="width: 200px; heigth:200px; cursor: pointer;" link @click="ver('espn2')" src="@/assets/img/logos/espn2.png" alt="">
+              </v-col>
+
+              <v-col cols="3" md="3" align="center" style="display: flex; align-items: center;">
+                <img style="width: 200px; heigth:200px; cursor: pointer;" link @click="ver('espn3')" src="@/assets/img/logos/espn3.png" alt="">
+              </v-col>
+
+              <v-col cols="3" md="3" align="center" style="display: flex; align-items: center;">
+                <img style="width: 200px; heigth:200px; cursor: pointer;" link @click="ver('dtvSports')" src="@/assets/img/logos/dtvSports.png" alt="">
+              </v-col>
+
+              <v-col cols="3" md="3" align="center" style="display: flex; align-items: center;">
+                <img style="width: 200px; heigth:200px; cursor: pointer;" link @click="ver('foxSports')" src="@/assets/img/logos/foxSports.png" alt="">
+              </v-col>
+
+              <v-col cols="3" md="3" align="center" style="display: flex; align-items: center;">
+                <img style="width: 200px; heigth:200px; cursor: pointer;" link @click="ver('tvperu')" src="@/assets/img/logos/tvperu.png" alt="">
+              </v-col>
+
+              <v-col cols="3" md="3" align="center" style="display: flex; align-items: center;">
+                <img style="width: 200px; heigth:200px; cursor: pointer;" link @click="ver('panamericana')" src="@/assets/img/logos/panamericana.png" alt="">
+              </v-col>
+
+              <v-col cols="3" md="3" align="center" style="display: flex; align-items: center;">
+                <img style="width: 200px; heigth:200px; cursor: pointer;" link @click="ver('telefe')" src="@/assets/img/logos/telefe.png" alt="">
+              </v-col>
+
+               <v-col cols="3" md="3" align="center" style="display: flex; align-items: center;">
+                <img style="width: 200px; heigth:200px; cursor: pointer;" link @click="ver('history')" src="@/assets/img/logos/history.png" alt="">
+              </v-col>
+
+              <v-col cols="3" md="3" align="center" style="display: flex; align-items: center;">
+                <img style="width: 200px; heigth:200px; cursor: pointer;" link @click="ver('history2')" src="@/assets/img/logos/history2.png" alt="">
+              </v-col>
+
+              <v-col cols="3" md="3" align="center" style="display: flex; align-items: center;">
+                <img style="width: 200px; heigth:200px; cursor: pointer;" link @click="ver('wchannel')" src="@/assets/img/logos/wchannel.png" alt="">
+              </v-col>
+
+               <v-col cols="3" md="3" align="center" style="display: flex; align-items: center;">
+                <img style="width: 200px; heigth:200px; cursor: pointer;" link @click="ver('nichelodeon')" src="@/assets/img/logos/nichelodeon.png" alt="">
+              </v-col>
+
+               <v-col cols="3" md="3" align="center" style="display: flex; align-items: center;">
+                <img style="width: 200px; heigth:200px; cursor: pointer;" link @click="ver('space')" src="@/assets/img/logos/space.png" alt="">
+              </v-col>
+
+              <v-col cols="3" md="3" align="center" style="display: flex; align-items: center;">
+                <img style="width: 200px; heigth:200px; cursor: pointer;" link @click="ver('tntSeries')" src="@/assets/img/logos/tntSeries.png" alt="">
+              </v-col>
+
+              <v-col cols="3" md="3" align="center" style="display: flex; align-items: center;">
+                <img style="width: 200px; heigth:200px; cursor: pointer;" link @click="ver('cinemax')" src="@/assets/img/logos/cinemax.png" alt="">
+              </v-col>
+
+               <v-col cols="3" md="3" align="center" style="display: flex; align-items: center;">
+                <img style="width: 200px; heigth:200px; cursor: pointer;" link @click="ver('sony')" src="@/assets/img/logos/sony.png" alt="">
+              </v-col>
+
+               <v-col cols="3" md="3" align="center" style="display: flex; align-items: center;">
+                <img style="width: 200px; heigth:200px; cursor: pointer;" link @click="ver('cinear')" src="@/assets/img/logos/cinear.png" alt="">
+              </v-col>
+
+              <v-col cols="3" md="3" align="center" style="display: flex; align-items: center;">
+                <img style="width: 200px; heigth:200px; cursor: pointer;" link @click="ver('universal')" src="@/assets/img/logos/universal.png" alt="">
+              </v-col>
+
+              <v-col cols="3" md="3" align="center" style="display: flex; align-items: center;">
+                <img style="width: 200px; heigth:200px; cursor: pointer;" link @click="ver('paramount')" src="@/assets/img/logos/paramount.png" alt="">
+              </v-col>
+
+              <v-col cols="3" md="3" align="center" style="display: flex; align-items: center;">
+                <img style="width: 200px; heigth:200px; cursor: pointer;" link @click="ver('starChannel')" src="@/assets/img/logos/starChannel.png" alt="">
+              </v-col>
+
+              <v-col cols="3" md="3" align="center" style="display: flex; align-items: center;">
+                <img style="width: 200px; heigth:200px; cursor: pointer;" link @click="ver('starChannelTv')" src="@/assets/img/logos/starChannelTv.png" alt="">
+              </v-col>
+
+              
+
+              
+
+             
+
+            </v-row>
+            
+          </div>
+          
+        </div>
+      </div>
+     
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  components: {
+
+  },
+  name: "index",
+  bodyClass: "index-page",
+  props: {
+    image: {
+      type: String,
+      default: require("@/assets/img/background_tv_cable.jpg")
+    },
+    leaf4: {
+      type: String,
+      default: require("@/assets/img/leaf4.png")
+    },
+    leaf3: {
+      type: String,
+      default: require("@/assets/img/leaf3.png")
+    },
+    leaf2: {
+      type: String,
+      default: require("@/assets/img/leaf2.png")
+    },
+    leaf1: {
+      type: String,
+      default: require("@/assets/img/leaf1.png")
+    },
+    signup: {
+      type: String,
+      default: require("@/assets/img/city.jpg")
+    },
+    landing: {
+      type: String,
+      default: require("@/assets/img/landing.jpg")
+    },
+    profile: {
+      type: String,
+      default: require("@/assets/img/profile.jpg")
+    }
+  },
+  data() {
+    return {
+      firstname: null,
+      email: null,
+      password: null,
+      leafShow: false
+    };
+  },
+  methods: {
+    leafActive() {
+      if (window.innerWidth < 768) {
+        this.leafShow = false;
+      } else {
+        this.leafShow = true;
+      }
+    },
+    ver(canal){
+      this.$router.push(
+        {
+          name: "channel",
+          params: { channelId: canal },
+        },
+        () => {}
+      );
+    }
+
+  },
+  computed: {
+    headerStyle() {
+      return {
+        backgroundImage: `url(${this.image})`
+      };
+    },
+    signupImage() {
+      return {
+        backgroundImage: `url(${this.signup})`
+      };
+    }
+  },
+  mounted() {
+    this.leafActive();
+    window.addEventListener("resize", this.leafActive);
+  },
+  beforeDestroy() {
+    window.removeEventListener("resize", this.leafActive);
+  }
+};
+</script>
+<style lang="scss">
+.section-download {
+  .md-button + .md-button {
+    margin-left: 5px;
+  }
+}
+
+@media all and (min-width: 991px) {
+  .btn-container {
+    display: flex;
+  }
+}
+</style>
---
+++ src/views/Landing.vue
@@ -0,0 +1,221 @@
+<template>
+  <div class="wrapper">
+    <parallax class="section page-header header-filter" :style="headerStyle">
+      <div class="container">
+        <div class="md-layout">
+
+          <div class="md-layout-item" v-if="channel === 'discoveryTurbo'">
+            <h1 class="title">Discovery Turbo</h1>
+            <iframe style="height: 563px; width:1000px" allow="encrypted-media" width="100%" height="1000px !important;" src="https://tv7.mitele.uno/embed/mpd.php?canal=Discovery Turbo&amp;stream=/live/live/DiscoveryTurbo/SA_Live_dash_enc_2A/DiscoveryTurbo.mpd" id="iframe" name="iframe" frameborder="0" scrolling="yes" allowfullscreen=""></iframe>           
+          </div>
+
+          <div class="md-layout-item" v-if="channel === 'h&h'">
+            <h1 class="title">Home & Health</h1>
+            <iframe style="height: 563px; width:1000px" allow="encrypted-media" width="100%" height="100%" src="https://tv7.mitele.uno/embed/mpd.php?canal=Discovery home and health&amp;stream=/live/c3eds/DiscoveryHomeHealthHD/SA_Live_dash_enc_2A/DiscoveryHomeHealthHD.mpd" id="iframe" name="iframe" frameborder="0" scrolling="yes" allowfullscreen=""></iframe>
+          </div>
+
+          <div class="md-layout-item" v-if="channel === 'discoveryID'">
+            <h1 class="title">Discovery ID</h1>
+            <iframe style="height: 563px; width:1000px" allow="encrypted-media" width="100%" height="100%" src="https://tv7.mitele.uno/embed/mpd.php?canal=Discovery ID&amp;stream=/live/live/ID/SA_Live_dash_enc_2A/ID.mpd" id="iframe" name="iframe" frameborder="0" scrolling="yes" allowfullscreen=""></iframe>
+          </div>
+
+          <div class="md-layout-item" v-if="channel === 'discoveryChannel'">
+            <h1 class="title">Discovery Channel</h1>
+            <iframe style="height: 563px; width:1000px" allow="encrypted-media" width="100%" height="100%" src="https://tv7.mitele.uno/embed/mpd.php?canal=Discovery Channel&amp;stream=/live/c3eds/DiscoveryHD/SA_Live_dash_enc_2A/DiscoveryHD.mpd" id="iframe" name="iframe" frameborder="0" scrolling="yes" allowfullscreen=""></iframe>
+          </div>
+
+          <div class="md-layout-item" v-if="channel === 'discoveryTheater'">
+            <h1 class="title">Discovery Theater</h1>
+            <iframe style="height: 563px; width:1000px" allow="encrypted-media" width="100%" height="100%" src="https://tv7.mitele.uno/embed/mpd.php?canal=Discovery Theater&amp;stream=/live/c2live/Theater_HD/SA_Live_dash_enc_2A/Theater_HD.mpd" id="iframe" name="iframe" frameborder="0" scrolling="yes" allowfullscreen=""></iframe>
+          </div>
+
+          <div class="md-layout-item" v-if="channel === 'natgeo'">
+            <h1 class="title">National Geographic</h1>
+            <iframe style="height: 563px; width:1000px" allow="encrypted-media" width="100%" height="100%" src="https://tv7.mitele.uno/embed/mpd.php?canal=Natgeo&amp;stream=/live/c3eds/NatGeoHD/SA_Live_dash_enc_2A/NatGeoHD.mpd" id="iframe" name="iframe" frameborder="0" scrolling="yes" allowfullscreen=""></iframe>
+          </div>
+
+          <div class="md-layout-item" v-if="channel === 'axn'">
+            <h1 class="title">AXN</h1>
+            <iframe style="height: 563px; width:1000px" allow="encrypted-media" width="100%" height="100%" src="https://tv7.mitele.uno/embed/mpd.php?canal=AXN&amp;stream=/live/live/AXNHD/SA_Live_dash_enc_2A/AXNHD.mpd" id="iframe" name="iframe" frameborder="0" scrolling="yes" allowfullscreen=""></iframe>
+          </div>
+
+          <div class="md-layout-item" v-if="channel === 'vtv'">
+            <h1 class="title">VTV</h1>
+            <iframe style="height: 563px; width:1000px" class="embed-responsive-item" src="https://jquick-js.github.io/kid/?u=aHR0cHM6Ly9lcy1jZG4uY29tL3Z0di5qcz9hZGZmZ2Rk" scrolling="no" frameborder="0" allow="autoplay *; encrypted-media *;" allowfullscreen=""></iframe>
+          </div>
+
+          <div class="md-layout-item" v-if="channel === 'espn2'">
+            <h1 class="title">ESPN 2</h1>
+            <iframe style="height: 563px; width:1000px" allow="encrypted-media" width="100%" height="100%" src="https://tv7.mitele.uno/embed/mpd.php?canal=ESPN 2&amp;stream=live/c2live/ESPN_HD/SA_Live_dash_enc_2A/ESPN_HD.mpd" id="iframe" name="iframe" frameborder="0" scrolling="no" allowfullscreen=""></iframe>
+          </div>
+
+          <div class="md-layout-item" v-if="channel === 'espn3'">
+            <h1 class="title">ESPN 3</h1>
+            <iframe style="height: 563px; width:1000px" allow="encrypted-media" width="100%" height="100%" src="https://tv7.mitele.uno/embed/mpd.php?canal=ESPN 3&amp;stream=/live/c3eds/ESPN3/SA_Live_dash_enc_2A/ESPN3.mpd" id="iframe" name="iframe" frameborder="0" scrolling="no" allowfullscreen=""></iframe>
+          </div>
+
+          <div class="md-layout-item" v-if="channel === 'dtvSports'">
+            <h1 class="title">Directv Sports</h1>
+            <iframe style="height: 563px; width:1000px" allow="encrypted-media" width="100%" height="100%" src="https://www.mitele.uno/embed/dtv/dtv1.php" id="iframe" name="iframe" frameborder="0" scrolling="no" allowfullscreen=""></iframe>
+          </div>
+
+          <div class="md-layout-item" v-if="channel === 'foxSports'">
+            <h1 class="title">Fox Sports</h1>
+            <iframe style="height: 563px; width:1000px" allow="encrypted-media" width="100%" height="100%" src="https://tv7.mitele.uno/embed/mpd.php?canal=Fox Sports&amp;stream=/live/c3eds/FoxSports/SA_Live_dash_enc_2A/FoxSports.mpd" id="iframe" name="iframe" frameborder="0" scrolling="no" allowfullscreen=""></iframe>
+          </div>
+
+          <div class="md-layout-item" v-if="channel === 'tvperu'">
+            <h1 class="title">TV Perú</h1>
+            <iframe style="height: 563px; width:1000px" allow="encrypted-media" width="100%" height="100%" src="https://player.teleringa.com/embed/ext.php?url=cdnh8.iblups.com/hls/R9WtilpKKB.m3u8" id="iframe" name="iframe" frameborder="0" scrolling="no" allowfullscreen=""></iframe>
+          </div>
+
+           <div class="md-layout-item" v-if="channel === 'panamericana'">
+            <h1 class="title">Panamericana Perú</h1>
+            <iframe style="height: 563px; width:1000px" allow="encrypted-media" width="100%" height="100%" src="https://player.teleringa.com/embed/ext.php?url=cdnhd.iblups.com/hls/ptv2.m3u8" id="iframe" name="iframe" frameborder="0" scrolling="no" allowfullscreen=""></iframe>
+          </div>
+
+           <div class="md-layout-item" v-if="channel === 'history'">
+            <h1 class="title">History Channel</h1>
+            <iframe style="height: 563px; width:1000px" allow="encrypted-media" width="100%" height="100%" src="https://tv7.mitele.uno/embed/mpd.php?canal=History&amp;stream=/live/live/HistoryHD/SA_Live_dash_enc_2A/HistoryHD.mpd" id="iframe" name="iframe" frameborder="0" scrolling="yes" allowfullscreen=""></iframe>
+          </div>
+
+           <div class="md-layout-item" v-if="channel === 'history2'">
+            <h1 class="title">History Channel 2</h1>
+            <iframe style="height: 563px; width:1000px" allow="encrypted-media" width="100%" height="100%" src="https://tv7.mitele.uno/embed/mpd.php?canal=History 2&amp;stream=/live/c2live/History_2/SA_Live_dash_enc_2A/History_2.mpd" id="iframe" name="iframe" frameborder="0" scrolling="yes" allowfullscreen=""></iframe>
+          </div>
+
+           <div class="md-layout-item" v-if="channel === 'vtvplus'">
+            <h1 class="title">VTV Plus</h1>
+            <iframe style="height: 563px; width:1000px" src="http://tvporinternet.eshost.com.ar/canales/vtv.html" height="100%" width="100%" frameborder="0" allowfullscreen scrolling="no"></iframe>
+          </div>
+
+          <div class="md-layout-item" v-if="channel === 'telefe'">
+            <h1 class="title">Telefe</h1>
+            <iframe style="height: 563px; width:1000px" allow="encrypted-media" width="100%" height="100%" src="https://tv7.mitele.uno/embed/mpd-tlf.php?canal=Telefe&amp;stream=live/c3eds/TelefeHD/SA_Live_dash_enc_2A/TelefeHD.mpd" id="iframe" name="iframe" frameborder="0" scrolling="no" allowfullscreen=""></iframe>
+          </div>
+
+          <div class="md-layout-item" v-if="channel === 'wchannel'">
+            <h1 class="title">Warner Channel</h1>
+            <iframe style="height: 563px; width:1000px" allow="encrypted-media" width="100%" height="100%" src="https://tv7.mitele.uno/embed/mpd.php?canal=Warner Channel&amp;stream=/live/live/WarnerHD/SA_Live_dash_enc_2A/WarnerHD.mpd" id="iframe" name="iframe" frameborder="0" scrolling="yes" allowfullscreen=""></iframe>
+          </div>
+
+           <div class="md-layout-item" v-if="channel === 'nichelodeon'">
+            <h1 class="title">Nichelodeon</h1>
+            <iframe style="height: 563px; width:1000px" allow="encrypted-media" width="100%" height="100%" src="https://tv7.mitele.uno/embed/mpd.php?canal=Nick&amp;stream=/live/c3eds/Nickelodeon/SA_Live_dash_enc_2A/Nickelodeon.mpd" id="iframe" name="iframe" frameborder="0" scrolling="yes" allowfullscreen=""></iframe>
+          </div>
+
+           <div class="md-layout-item" v-if="channel === 'space'">
+            <h1 class="title">Space</h1>
+            <iframe style="height: 563px; width:1000px" allow="encrypted-media" width="100%" height="100%" src="https://tv7.mitele.uno/embed/mpd.php?canal=Space&amp;stream=/live/c3eds/Space/SA_Live_dash_enc_2A/Space.mpd" id="iframe" name="iframe" frameborder="0" scrolling="yes" allowfullscreen=""></iframe>
+          </div>
+
+          <div class="md-layout-item" v-if="channel === 'tntSeries'">
+            <h1 class="title">TNT Series</h1>
+            <iframe style="height: 563px; width:1000px" allow="encrypted-media" width="100%" height="100%" src="https://tv7.mitele.uno/embed/mpd.php?canal=TNT Series&amp;stream=/live/c3eds/TNTSeries/SA_Live_dash_enc_2A/TNTSeries.mpd" id="iframe" name="iframe" frameborder="0" scrolling="yes" allowfullscreen=""></iframe>
+          </div>
+
+          <div class="md-layout-item" v-if="channel === 'cinemax'">
+            <h1 class="title">Cinemax</h1>
+            <iframe style="height: 563px; width:1000px" allow="encrypted-media" width="100%" height="100%" src="https://tv7.mitele.uno/embed/mpd.php?canal=Cinemax&amp;stream=/live/c2live/Cinemax/SA_Live_dash_enc_2A/Cinemax.mpd" id="iframe" name="iframe" frameborder="0" scrolling="yes" allowfullscreen=""></iframe>
+          </div>
+
+           <div class="md-layout-item" v-if="channel === 'sony'">
+            <h1 class="title">Sony</h1>
+            <iframe style="height: 563px; width:1000px" allow="encrypted-media" width="100%" height="100%" src="https://tv7.mitele.uno/embed/mpd.php?canal=Sony&amp;stream=/live/live/SonyHD/SA_Live_dash_enc_2A/SonyHD.mpd" id="iframe" name="iframe" frameborder="0" scrolling="yes" allowfullscreen=""></iframe>
+          </div>
+
+           <div class="md-layout-item" v-if="channel === 'cinear'">
+            <h1 class="title">Cine Argentino</h1>
+            <iframe style="height: 563px; width:1000px" allow="encrypted-media" width="100%" height="100%" src="https://tv7.mitele.uno/embed/mpd.php?canal=CineAR&amp;stream=/live/c2live/INCAA_Tv/SA_Live_dash_enc_2A/INCAA_Tv.mpd" id="iframe" name="iframe" frameborder="0" scrolling="yes" allowfullscreen=""></iframe>
+          </div>
+
+          <div class="md-layout-item" v-if="channel === 'universal'">
+            <h1 class="title">Universal Channel</h1>
+            <iframe style="height: 563px; width:1000px" allow="encrypted-media" width="100%" height="100%" src="https://tv7.mitele.uno/embed/mpd.php?canal=Studio Universal&amp;stream=/live/c2live/Studio_Universal/SA_Live_dash_enc_2A/Studio_Universal.mpd" id="iframe" name="iframe" frameborder="0" scrolling="yes" allowfullscreen=""></iframe>
+          </div>
+
+          <div class="md-layout-item" v-if="channel === 'paramount'">
+            <h1 class="title">Paramount Channel</h1>
+            <iframe style="height: 563px; width:1000px" allow="encrypted-media" width="100%" height="100%" src="https://tv7.mitele.uno/embed/mpd.php?canal=Paramount&amp;stream=/live/live/Paramount/SA_Live_dash_enc_2A/Paramount.mpd" id="iframe" name="iframe" frameborder="0" scrolling="yes" allowfullscreen=""></iframe>
+          </div>
+
+          <div class="md-layout-item" v-if="channel === 'discoveryWorld'">
+            <h1 class="title">Discovery World</h1>
+            <iframe style="height: 563px; width:1000px" allow="encrypted-media" width="100%" height="100%" src="https://tv7.mitele.uno/embed/mpd.php?canal=Discovery World&amp;stream=live/c2live/Discovery_World_HD/SA_Live_dash_enc_2A/Discovery_World_HD.mpd" id="iframe" name="iframe" frameborder="0" scrolling="yes" allowfullscreen=""></iframe>
+          </div>
+
+          <div class="md-layout-item" v-if="channel === 'starChannel'">
+            <h1 class="title">Star Channel Series</h1>
+            <iframe style="height: 563px; width:1000px" allow="encrypted-media" width="100%" height="100%" src="https://tv7.mitele.uno/embed/mpd.php?canal=Star Series&amp;stream=/live/c3eds/FOX1HD/SA_Live_dash_enc_2A/FOX1HD.mpd" id="iframe" name="iframe" frameborder="1" scrolling="yes" allowfullscreen=""></iframe>
+          </div>
+
+          <div class="md-layout-item" v-if="channel === 'starChannelTv'">
+            <h1 class="title">Star Channel</h1>
+            <iframe style="height: 563px; width:1000px" allow="encrypted-media" width="100%" height="100%" src="https://tv7.mitele.uno/embed/mpd.php?canal=Star Channel&amp;stream=/live/c3eds/FOXHD/SA_Live_dash_enc_2A/FOXHD.mpd" id="iframe" name="iframe" frameborder="0" scrolling="yes" allowfullscreen=""></iframe>
+          </div>
+          
+
+        </div>
+      </div>
+    </parallax>
+   
+  </div>
+</template>
+
+<script>
+export default {
+  bodyClass: "landing-page",
+  props: {
+    header: {
+      type: String,
+      default: require("@/assets/img/background_landing.jpg")
+    },
+    teamImg1: {
+      type: String,
+      default: require("@/assets/img/faces/avatar.jpg")
+    },
+    teamImg2: {
+      type: String,
+      default: require("@/assets/img/faces/christian.jpg")
+    },
+    teamImg3: {
+      type: String,
+      default: require("@/assets/img/faces/kendall.jpg")
+    }
+  },
+  data() {
+    return {
+      name: null,
+      email: null,
+      message: null,
+      channel: null,
+    };
+  },
+  computed: {
+    headerStyle() {
+      return {
+        backgroundImage: `url(${this.header})`
+      };
+    }
+  },
+  created: function() {
+    this.channel = this.$route.params.channelId;
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.md-card-actions.text-center {
+  display: flex;
+  justify-content: center !important;
+}
+.contact-form {
+  margin-top: 30px;
+}
+
+.md-has-textarea + .md-layout {
+  margin-top: 15px;
+}
+
+
+</style>
